80b423791eaacbe23e22a01369d576218957ccfe
tmbconnect <tinabr@byui.edu>
2024-09-21 21:05:15 -0600

    Completed Prep3 activity.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // variable to hold number of times it took user to guess
+        int guessAttempts = 0;
+
+        Console.WriteLine("Let's Play the magic number game!");
+        Console.WriteLine("I will pick a number between 1- 100 and you guess what it is.");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            guessAttempts += 1;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations! The magic number is {magicNumber}");
+                Console.Write($"It took you {guessAttempts} to guess the magic number.");
+            }
+        }
+
     }
 }
