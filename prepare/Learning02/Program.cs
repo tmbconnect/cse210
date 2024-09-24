520f1613fe1c1b847694ea1f59e96f55f2028684
tmbconnect <tinabr@byui.edu>
2024-09-24 13:59:26 -0600

    restored program to see what I did wrong

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,13 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-       Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._statYear = "2019";
-        job1._endYear = "2022";
-
-        job1.Display();
-
+        Console.WriteLine(Hello Learning Activity2!);
     }
 }
