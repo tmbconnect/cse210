032edec55540968476a2c732333d70d7aba7104f
tmbconnect <tinabr@byui.edu>
2024-09-24 15:06:05 -0600

    Added Job.cs file, created Job class, created member variables for job class, added code to main function and created nwe intances named job1 and job 2 verified that veriables would display on the screen.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Hello Learning Activity2!);
+        //Create new job instance named job1.
+        Job job1 = new Job();
+        // Set member variables using the dot notation.
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        // verify you can display the company of job on the screen 
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
+
     }
 }
