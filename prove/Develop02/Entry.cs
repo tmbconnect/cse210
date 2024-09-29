baf891148b3af03e8103a58aaa020c887e14809d
tmbconnect <tinabr@byui.edu>
2024-09-28 23:53:12 -0600

    entry.cs  updates for prove assignment

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,15 +1,27 @@
 using System;
-
+// Public class that matches my .cs file
 public class Entry
 {
+    // Attributes
     public string _date;
     public string _promptText;
     public string _entryText;
 
+    // Constroctor to initialize the Entry object
+    public Entry(string date, string promptText, string entryText)
+    {
+        _date = date;
+        _promptText = promptText;
+        _entryText = entryText;
+    }
 
+    // Method to display the entry details
     public void Display()
     {
-         // add method behaviors here
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_promptText}");
+        Console.WriteLine($"Entry: {_entryText}");
+        Console.WriteLine();
     }
 
 
