e1bf296769fd9e52d82ed404c1f21a037bf02ec4
tmbconnect <tinabr@byui.edu>
2024-09-29 00:06:15 -0600

    Changes to Program class of the journal assignment for week 02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -19,15 +19,43 @@ class Program
            Console.WriteLine("4. Load the journal from a file");
            Console.WriteLine("5. Exit");
            string choice = Console.ReadLine();
-        }
+        
         switch (choice)
         {
-           case "1":
+            case "1":
                 string prompt = promptGenerator.GetRandomPrompt();
                 Console.WriteLine(prompt);
                 string entryText = Console.ReadLinde();
                 string date = DateTime.Now.ToString("yyyy-MM-dd");
-                
+                journal.AddEntry(newEntry(date, prompt, entryText));
+                break;
+
+            case "2":
+                journal.DisplayAll();
+                break;
+
+            case "3":
+                Console.Write("Enter filename to save: ");
+                string saveFilename = Console.ReadLine();
+                journal.SaveToFile(saveFilename);
+                break;
+
+            case "4":
+                Console.Write("Enter filename to load: ");
+                string loadFilename = Console.ReadLine();
+                journal.SaveToFile(saveFilename);
+                break;
+
+            case "5":
+                running = false;
+                break;
+
+            default:
+                Console.WriteLine("Invalid option, Please try again.");
+                break;
+
+           }    
+
         }
         
     }
