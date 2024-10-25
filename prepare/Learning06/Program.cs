b48b099dc8b4a2f900d0a85267f4bfc58c864ba6
tmbconnect <tinabr@byui.edu>
2024-10-24 18:42:45 -0600

    Tested Square, Rectangle, and Circle Classes. Called GetColor and GetArea method for each

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -3,7 +3,23 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning06 World!");
+    {   // Create Square instance, call GetColor() and GetArea() methods
+        Square mySquare = new Square("Pink", 5);
+        Console.WriteLine(mySquare.GetColor());
+        Console.WriteLine(mySquare.GetArea());
+        Console.WriteLine();
+
+        // Create Rectangle instance, call GetColor() and GetArea() methods
+        Rectangle myRectangle = new Rectangle("Purple", 10, 3);
+        Console.WriteLine(myRectangle.GetColor());
+        Console.WriteLine(myRectangle.GetArea());
+        Console.WriteLine();
+
+        // Create Circle instance, call GetColor() and GetArea() methods
+        Circle myCircle = new Circle("Green", 3);
+        Console.WriteLine(myCircle.GetColor());
+        Console.WriteLine(myCircle.GetArea());
+        Console.WriteLine();
+
     }
 }
